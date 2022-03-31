13a0527e6b69a3a0dcc4f57f9721de4c7b843434
wintezz <saberbringers@gmail.com>
2022-04-01 01:16:18 +0300

    merge

---
+++ app/src/main/java/com/example/online_market/App.kt
@@ -0,0 +1,7 @@
+package com.example.online_market
+
+import android.app.Application
+
+class App : Application() {
+
+}
---
+++ app/src/main/java/com/example/online_market/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.online_market
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val intent = Intent (this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        //
+    }
+}
